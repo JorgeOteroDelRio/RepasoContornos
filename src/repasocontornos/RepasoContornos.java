f0cf4df7b3abbd75522e73b6868ae3d3b6ff4844
Jorge <Jorge@192.168.0.10>
2016-05-08 11:57:16 +0200

    Repaso contornos

---
+++ src/repasocontornos/RepasoContornos.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repasocontornos;
+
+/**
+ *
+ * @author Jorge
+ */
+public class RepasoContornos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean aaronListo = false;
+        if(aaronListo){
+            System.out.println("Aaron saca 10");
+        }else{
+            System.out.println("Aaron suspende");
+        }
+    }
+    
+}
